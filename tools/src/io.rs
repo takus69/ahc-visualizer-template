58b4cd1bf2beaca0382cb02b9c09eb519726124e
terry-u16 <terry-u16@users.noreply.github.com>
2024-02-03 19:58:11 +0900

    AHC012をビジュアライズ

--- tools/src/io.rs
+++ tools/src/io.rs
@@ -1,6 +1,8 @@
 mod helpers;
 
+use anyhow::ensure;
 use helpers::read;
+use itertools::Itertools;
 use rand::{prelude::*, SeedableRng};
 use rand_chacha::ChaCha20Rng;
 use std::{fmt::Display, iter::Peekable};
@@ -15,37 +17,105 @@ pub struct GenOption {
 #[derive(Debug, Clone)]
 pub struct Input {
     pub n: usize,
+    pub k: usize,
+    pub a: Vec<usize>,
+    pub p: Vec<(i64, i64)>,
 }
 
 impl Input {
+    pub const CIRCLE_SIZE: i64 = 10000;
+    pub const CIRCLE_SIZE2: i64 = Self::CIRCLE_SIZE * Self::CIRCLE_SIZE;
+
     /// **(CUSTOMIZE IT!)** Generate Input
     pub fn gen(option: GenOption) -> Self {
         let mut rng = ChaCha20Rng::seed_from_u64(option.seed);
 
         // You shold generate u64 first and then convert it to usize because the size of usize is platform dependent.
-        let n = rng.gen_range(10..=15u64) as usize;
+        let mut a = vec![];
 
-        todo!("Write code to generate Input here.");
+        for _ in 1..=10 {
+            a.push(rng.gen_range(1..=100u64) as usize);
+        }
 
-        Self { n }
+        let n = a.iter().enumerate().map(|(i, ai)| (i + 1) * ai).sum();
+
+        let mut p = vec![];
+
+        for _ in 0..n {
+            let (x, y) = loop {
+                let x = rng.gen_range(-Self::CIRCLE_SIZE..=Self::CIRCLE_SIZE);
+                let y = rng.gen_range(-Self::CIRCLE_SIZE..=Self::CIRCLE_SIZE);
+
+                if x * x + y * y >= Self::CIRCLE_SIZE2 {
+                    continue;
+                }
+
+                let mut ok = true;
+
+                for (xi, yi) in p.iter() {
+                    let dx = x - xi;
+                    let dy = y - yi;
+
+                    if dx * dx + dy * dy <= 10 * 10 {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if ok {
+                    break (x, y);
+                }
+            };
+
+            p.push((x, y));
+        }
+
+        let k = 100;
+
+        Self { n, k, a, p }
     }
 
     /// **(CUSTOMIZE IT!)** Parse Input from tokens
     pub(super) fn parse<'a>(tokens: &mut impl Iterator<Item = &'a str>) -> anyhow::Result<Self> {
-        let n = read(tokens.next(), 0, 10)?;
+        let n = read(tokens.next(), 0, 10000)?;
+        let k = read(tokens.next(), 100, 100)?;
+        let mut a = vec![];
 
-        todo!("Write code to parse Input here.");
+        for _ in 0..10 {
+            a.push(read(tokens.next(), 1, 100)?);
+        }
 
-        Ok(Self { n })
+        let mut p = vec![];
+
+        for _ in 0..n {
+            let x = read(tokens.next(), -Self::CIRCLE_SIZE, Self::CIRCLE_SIZE)?;
+            let y = read(tokens.next(), -Self::CIRCLE_SIZE, Self::CIRCLE_SIZE)?;
+
+            p.push((x, y));
+        }
+
+        let sum_a = a
+            .iter()
+            .enumerate()
+            .map(|(i, ai)| (i + 1) * ai)
+            .sum::<usize>();
+        ensure!(sum_a == n, "sum_a = {} but n = {}", sum_a, n);
+
+        Ok(Self { n, k, a, p })
     }
 }
 
 impl Display for Input {
     /// **(CUSTOMIZE IT!)** Format Input as string
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "{}", self.n)?;
+        writeln!(f, "{} {}", self.n, self.k)?;
 
-        todo!("Write code to format Input here.");
+        let a = self.a.iter().map(usize::to_string).join(" ");
+        writeln!(f, "{}", a)?;
+
+        for (x, y) in self.p.iter() {
+            writeln!(f, "{} {}", x, y)?;
+        }
 
         Ok(())
     }
@@ -55,6 +125,7 @@ impl Display for Input {
 #[derive(Debug, Clone)]
 pub struct Output {
     pub k: usize,
+    pub cut: Vec<(i64, i64, i64, i64)>,
 }
 
 impl Output {
@@ -63,19 +134,92 @@ impl Output {
         input: &Input,
         tokens: &mut Peekable<impl Iterator<Item = &'a str>>,
     ) -> anyhow::Result<Self> {
-        let k = read(tokens.next(), 0, 1000)?;
+        let k = read(tokens.next(), 0, input.k)?;
 
-        todo!("Write code to parse Output here.");
+        let mut cut = vec![];
 
-        Ok(Self { k })
+        const LIMIT: i64 = 100000000;
+
+        for _ in 0..k {
+            let x1 = read(tokens.next(), -LIMIT, LIMIT)?;
+            let y1 = read(tokens.next(), -LIMIT, LIMIT)?;
+            let x2 = read(tokens.next(), -LIMIT, LIMIT)?;
+            let y2 = read(tokens.next(), -LIMIT, LIMIT)?;
+
+            cut.push((x1, y1, x2, y2));
+        }
+
+        Ok(Self { k, cut })
     }
 
     /// **(CUSTOMIZE IT!)** Calculate score
     pub(super) fn calc_score(&self, input: &Input) -> anyhow::Result<i64> {
-        let score = 100;
+        let b = self.get_b(input);
 
-        todo!("Write code to calculate score here.");
+        let mut ab_sum = 0;
+        let mut a_sum = 0;
+
+        for i in 0..10 {
+            ab_sum += input.a[i].min(b[i]);
+            a_sum += input.a[i];
+        }
+
+        let score = (1e6 * ab_sum as f64 / a_sum as f64).round() as i64;
 
         Ok(score)
+    }
+
+    pub fn get_b(&self, input: &Input) -> Vec<usize> {
+        let groups = self.div_cakes(input);
+        let mut b = vec![0; 10];
+
+        for group in groups {
+            if group.len() - 1 < b.len() {
+                b[group.len() - 1] += 1;
+            }
+        }
+
+        b
+    }
+
+    pub fn div_cakes(&self, input: &Input) -> Vec<Vec<(i64, i64)>> {
+        let mut groups = vec![input.p.clone()];
+
+        for (x1, y1, x2, y2) in self.cut.iter() {
+            let mut new_group = vec![];
+
+            let vx1 = x2 - x1;
+            let vy1 = y2 - y1;
+
+            for group in &groups {
+                let mut g0 = vec![];
+                let mut g1 = vec![];
+
+                for &(x, y) in group {
+                    let vx2 = x - x1;
+                    let vy2 = y - y1;
+
+                    let cross = vx1 * vy2 - vx2 * vy1;
+
+                    if cross < 0 {
+                        g0.push((x, y));
+                    } else if cross > 0 {
+                        g1.push((x, y));
+                    }
+                }
+
+                if !g0.is_empty() {
+                    new_group.push(g0);
+                }
+
+                if !g1.is_empty() {
+                    new_group.push(g1);
+                }
+            }
+
+            groups = new_group;
+        }
+
+        groups
     }
 }
--- tools/src/vis.rs
+++ tools/src/vis.rs
@@ -1,10 +1,14 @@
 mod helpers;
 
+use std::collections::HashSet;
+
 use crate::{
     io::{Input, Output},
-    vis::helpers::{create_circle, create_rect, get_color, init_svg, with_title},
+    vis::helpers::{create_circle, create_rect, create_text, get_color, init_svg, Color, Stroke},
 };
 use svg::Document;
+
+use self::helpers::create_line;
 
 /// **(CUSTOMIZE IT!)**  Option for visualization
 #[derive(Debug, Clone)]
@@ -19,7 +23,9 @@ pub struct VisResult {
     pub svg: Document,
 }
 
-const VIEW_SIZE: f64 = 600.0;
+const VIEW_OFFSET_Y: f64 = 100.0;
+const VIEW_WIDTH: f64 = 600.0;
+const VIEW_HEIGHT: f64 = VIEW_WIDTH + VIEW_OFFSET_Y;
 const VIEW_PADDING: f64 = 10.0;
 
 /// **(CUSTOMIZE IT!)** Visualize the output
@@ -32,27 +38,162 @@ pub(super) fn visualize(
         turn: outputs.len() - 1,
     });
 
-    let mut doc = init_svg(VIEW_SIZE, VIEW_PADDING);
+    let mut doc = init_svg(VIEW_WIDTH, VIEW_HEIGHT, VIEW_PADDING);
 
-    // Draw Input
-    let x = 30.0 * (input.n as f64 + 1.0);
-    doc = doc.add(create_rect(x, 10.0, 60.0, 60.0, Some(get_color(0.5)), None));
+    let output = if outputs.len() == 0 {
+        Output { k: 0, cut: vec![] }
+    } else {
+        outputs[option.turn].clone()
+    };
 
-    todo!("Write code to visualize here.");
+    doc = draw_cake(doc);
+    doc = draw_lines(&output, doc);
+    doc = draw_strawberry(input, &output, doc);
+    doc = draw_table(input, &output, doc);
 
-    // Draw Output
-    if outputs.len() == 0 {
-        return Ok(VisResult { score: 0, svg: doc });
-    }
-
-    let output = &outputs[option.turn];
-    let score = output.calc_score(input)?;
-
-    let y = 10.0 * (output.k as f64 + 1.0);
-    doc = doc.add(with_title(
-        create_circle(200., y, 20., Some("gray".into()), None),
-        "hoge",
-    ));
+    let score = output.calc_score(&input)?;
 
     Ok(VisResult { score, svg: doc })
+}
+
+fn draw_table(input: &Input, output: &Output, mut doc: Document) -> Document {
+    const X0: f64 = 25.0;
+    const Y0: f64 = 10.0;
+    const W: f64 = 50.0;
+    const H: f64 = 30.0;
+
+    let b = output.get_b(input);
+
+    for i in 0..3 {
+        for j in 0..=10 {
+            let x0 = X0 + W * j as f64;
+            let y0 = Y0 + H * i as f64;
+
+            let color = if i == 0 && j > 0 {
+                get_color((j - 1) as f64 / 9.0)
+            } else {
+                Color::Name("white".into())
+            };
+
+            doc = doc.add(create_rect(
+                x0,
+                y0,
+                W,
+                H,
+                Some(color),
+                Some(Stroke("black".into(), 1.0)),
+            ));
+
+            let text = if j == 0 {
+                ["d", "a", "b"][i].to_string()
+            } else if i == 0 {
+                j.to_string()
+            } else if i == 1 {
+                input.a[j - 1].to_string()
+            } else {
+                b[j - 1].to_string()
+            };
+
+            let x = x0 + W / 2.0;
+            let y = y0 + H * 4.0 / 5.0;
+
+            doc = doc.add(create_text(x, y, 15.0, text));
+        }
+    }
+
+    doc
+}
+
+fn draw_cake(mut doc: Document) -> Document {
+    let center_x = VIEW_WIDTH * 0.5;
+    let center_y = VIEW_OFFSET_Y + VIEW_WIDTH * 0.5;
+    let scale = 280.0 / 1e4;
+
+    doc = doc.add(create_circle(
+        center_x,
+        center_y,
+        1e4 * scale,
+        Some("white".into()),
+        Some(Stroke("black".into(), 2.0)),
+    ));
+
+    doc
+}
+
+fn draw_lines(output: &Output, mut doc: Document) -> Document {
+    let center_x = VIEW_WIDTH * 0.5;
+    let center_y = VIEW_OFFSET_Y + VIEW_WIDTH * 0.5;
+    let scale = 280.0 / 1e4;
+
+    for &(x1, y1, x2, y2) in &output.cut {
+        let x1 = center_x + x1 as f64 * scale;
+        let y1 = center_y + y1 as f64 * scale;
+        let x2 = center_x + x2 as f64 * scale;
+        let y2 = center_y + y2 as f64 * scale;
+        doc = doc.add(create_line(x1, y1, x2, y2, 1.0, "black".into()))
+    }
+
+    doc
+}
+
+fn draw_strawberry(input: &Input, output: &Output, mut doc: Document) -> Document {
+    let center_x = VIEW_WIDTH * 0.5;
+    let center_y = VIEW_OFFSET_Y + VIEW_WIDTH * 0.5;
+    let scale = 280.0 / 1e4;
+
+    let groups = output.div_cakes(input);
+    let mut found = HashSet::new();
+
+    for group in groups {
+        let color = if group.len() >= 1 && group.len() <= 10 {
+            get_color((group.len() - 1) as f64 / 9.0)
+        } else {
+            Color::Name("white".into())
+        };
+
+        for &(x, y) in &group {
+            found.insert((x, y));
+        }
+
+        doc = draw_group(&group, center_x, scale, center_y, color, doc);
+    }
+
+    for &(x, y) in &input.p {
+        if !found.contains(&(x, y)) {
+            doc = draw_group(
+                &vec![(x, y)],
+                center_x,
+                scale,
+                center_y,
+                Color::Name("white".into()),
+                doc,
+            );
+        }
+    }
+
+    doc
+}
+
+fn draw_group(
+    group: &[(i64, i64)],
+    center_x: f64,
+    scale: f64,
+    center_y: f64,
+    color: Color,
+    mut doc: Document,
+) -> Document {
+    for &(x, y) in group {
+        let x = center_x + x as f64 * scale;
+        let y = center_y + y as f64 * scale;
+
+        doc = doc.add(create_circle(
+            x,
+            y,
+            3.0,
+            Some(color),
+            Some(Stroke("gray".into(), 1.0)),
+        ));
+    }
+
+    doc
 }
